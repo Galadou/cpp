f257ce3de7bae9ca21ddb8a4ccf12c9d571eb489
Galaad Mersch <gmersch@1G3.42angouleme.fr>
2025-03-08 19:46:45 +0100

    ex00 cpp08, main isnt finish

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -2,8 +2,17 @@
 #define EASYFIND_HPP
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 template <typename T>
-class
+void easyfind(T &param, int nb)
+{
+	typename T::iterator pos;
+
+	pos = std::find(param.begin(), param.end(), nb);
+	if (pos == param.end())
+		throw std::invalid_argument("The number isn't in this container.");
+}
 
 #endif
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,40 @@
+#include "easyfind.hpp"
+
+int main()
+{
+	std::cout << "for vector of int:" << std::endl;
+	try
+	{
+		std::vector<int> my_vector;
+		int nb = 42;
+
+		for (int i = 0; i < 5; i++)
+			my_vector.push_back(i);
+		my_vector.push_back(nb);
+		easyfind(my_vector, nb);
+		std::cout << "The number " << nb << " has been find inside my_vector !" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+
+		//ICI
+	try
+	{
+		std::vector<int> my_vector;
+		int nb = 42;
+
+		for (int i = 0; i < 5; i++)
+			my_vector.push_back(i);
+		my_vector.push_back(nb);
+		easyfind(my_vector, nb);
+		std::cout << "The number " << nb << " has been find inside my_vector !" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+		
+}
