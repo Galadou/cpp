cb1ced6e771ebd99307c3c47c7951ef346b44c71
Galaad Mersch <gmersch@2E1.42angouleme.fr>
2025-03-09 16:02:00 +0100

    cpp08 ex00 finish

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	std::cout << "for vector of int:" << std::endl;
+	std::cout << "For vector of int:" << std::endl;
 	try
 	{
 		std::vector<int> my_vector;
@@ -18,23 +18,4 @@ int main()
 	{
 		std::cerr << e.what() << '\n';
 	}
-
-
-		//ICI
-	try
-	{
-		std::vector<int> my_vector;
-		int nb = 42;
-
-		for (int i = 0; i < 5; i++)
-			my_vector.push_back(i);
-		my_vector.push_back(nb);
-		easyfind(my_vector, nb);
-		std::cout << "The number " << nb << " has been find inside my_vector !" << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-		
 }
